f56cac77d67237a3e09db101750210ef2b7ea5d6
jamesjosol <james.josol70@gmail.com>
2020-09-16 19:03:34 +0800

    second commit

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,9 +5,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel Prelim Demo</title>
     <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+    <style type="text/css">
+        .nav- {
+            background-color: rgba(0,0,0,0.7);
+            height: 75px;
+        }
+        .nav-con {
+            margin-left:65%;
+        }
+
+        .nav-con a{
+            display: inline;
+            color: white;
+            transition: all 0.4s ease 0s;
+        }
+    </style>
 </head>
 <body>
     <h1>Laravel Demo</h1>
-    Home | <a href="{{url('/profile')}}">Profile</a> | <a href="{{url('/about')}}">About Us</a>
+    <nav class="navbar navbar-expand-lg navbar-dark nav-">
+        <a class="navbar-brand" href="#"><h4>Home</h4></a>
+        <div class="nav-con">
+            <a class="nav-link" >Home</a>
+            <a class="nav-link" href="{{url('/profile')}}">Profile</a>
+            <a class="nav-link" href="{{url('/about')}}">About Us</a>
+        </div>
+    </nav>
+    
+    
+    <!--Home | <a class="" href="{{url('/profile')}}">Profile</a> | <a  class="nav-item nav-link" href="{{url('/about')}}">About Us</a>-->
+       
+    
+    
+    
+    
 </body>
 </html>
--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -5,10 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel Prelim Demo</title>
     <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+    <style type="text/css">
+        .nav- {
+            background-color: rgba(0,0,0,0.7);
+            height: 75px;
+        }
+        .nav-con {
+            margin-left:65%;
+        }
+
+        .nav-con a{
+            display: inline;
+            color: white;
+            transition: all 0.4s ease 0s;
+        }
+    </style>
 </head>
 <body>
-    <h1>Profile Page</h1>
-    <a href="{{url('/')}}">Home</a> | Profile | <a href="{{url('/about')}}">About Us</a>
+    <h1>Laravel Demo</h1>
+    <nav class="navbar navbar-expand-lg navbar-dark nav-">
+        <a class="navbar-brand" href="#"><h4>Profile</h4></a>
+        <div class="nav-con">
+            <a class="nav-link" href="{{url('/')}}">Home</a>
+            <a class="nav-link">Profile</a>
+            <a class="nav-link" href="{{url('/about')}}">About Us</a>
+        </div>
+    </nav>
+
 
 </body>
 </html>
